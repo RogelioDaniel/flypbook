c03f8cfd45aef6255fdfe6eb8708ee41926dd597
RogelioDaniel <rogelio-daniel@hotmail.es>
2023-05-11 18:47:41 -0600

    Juego

--- lib/main.dart
+++ lib/main.dart
@@ -1,199 +1,93 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
-import 'package:intl/intl.dart';
+import 'package:flutter/gestures.dart';
 
-void main() {
-  runApp(APODApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+  runApp(GameWrapper());
 }
 
-class APODApp extends StatelessWidget {
+class MyGame extends Game {
+  late Size screenSize;
+  late Rect characterRect;
+  late Rect enemyRect;
+  late double tileSize;
+  late Paint characterPaint;
+  late Paint enemyPaint;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'APOD NASA',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: APODHomePage(),
+  Future<void> onLoad() async {
+    characterPaint = Paint()..color = Colors.blue;
+    enemyPaint = Paint()..color = Colors.red;
+
+    characterRect = Rect.fromLTWH(
+      0,
+      0,
+      tileSize,
+      tileSize,
+    );
+
+    enemyRect = Rect.fromLTWH(
+      screenSize.width - tileSize,
+      screenSize.height - tileSize,
+      tileSize,
+      tileSize,
     );
   }
-}
 
-class APODHomePage extends StatefulWidget {
   @override
-  _APODHomePageState createState() => _APODHomePageState();
-}
+  void render(Canvas canvas) {
+    canvas.drawRect(characterRect, characterPaint);
+    canvas.drawRect(enemyRect, enemyPaint);
+  }
 
-class _APODHomePageState extends State<APODHomePage> {
-  List<String> _apodImageUrls = [];
-  List<DateTime> _apodDates = [];
-  String _apodDescription = '';
-  DateTime _selectedDate = DateTime.now();
+  @override
+  void update(double dt) {}
 
-  Future<void> _fetchAPOD(DateTime date) async {
-    final apiKey =
-        'sB7OLy9bfQSZN60awfxG02UjGkIPPsQITiaPEBGE'; // Replace with your NASA API key
-    final formattedDate = DateFormat('yyyy-MM-dd').format(date);
+  @override
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 9;
+  }
 
-    final url = Uri.parse(
-        'https://api.nasa.gov/planetary/apod?api_key=$apiKey&date=$formattedDate');
+  void onTapDown(TapDownDetails d) {
+    double x = d.globalPosition.dx;
+    double y = d.globalPosition.dy;
 
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final decodedResponse = json.decode(response.body);
-      setState(() {
-        _apodImageUrls.add(decodedResponse['url']);
-        _apodDates.add(date);
-        _apodDescription = decodedResponse['explanation'];
-      });
-    } else {
-      throw Exception('Failed to fetch APOD');
+    if (characterRect.contains(Offset(x, y))) {
+      print('Character tapped!');
     }
   }
 
-  Future<void> _setWallpaper(String imageUrl) async {
-    final appDocDir = await getApplicationDocumentsDirectory();
-    final wallpaperDir = Directory('${appDocDir.path}/NASAaPOD');
-    if (!wallpaperDir.existsSync()) {
-      wallpaperDir.createSync();
-    }
-
-    final fileName = _selectedDate.toString().split(' ')[0] + '.jpg';
-    final filePath = '${wallpaperDir.path}/$fileName';
-
-    final response = await http.get(Uri.parse(imageUrl));
-    final File imageFile = File(filePath);
-    await imageFile.writeAsBytes(response.bodyBytes);
-
-    final wallpaperResult = await WallpaperManager.setWallpaperFromFile(
-      filePath,
-      WallpaperManager.HOME_SCREEN,
-    );
-    print(wallpaperResult);
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Wallpaper changed successfully!'),
-      ),
-    );
+  void onPanUpdate(DragUpdateDetails d) {
+    characterRect = characterRect.translate(d.delta.dx, d.delta.dy);
   }
+}
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchAPOD(_selectedDate);
-    _fetchAPOD(_selectedDate.subtract(Duration(days: 1)));
-    _fetchAPOD(_selectedDate.subtract(Duration(days: 2)));
-  }
-
+class GameWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('APOD NASA'),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 16.0),
-            child: Text(
-              DateFormat('MMMM d, yyyy').format(_selectedDate),
-              style: TextStyle(fontSize: 16),
+    return GestureDetector(
+      onTapDown: (TapDownDetails details) {
+        MyGame().onTapDown(details);
+      },
+      onPanUpdate: (DragUpdateDetails details) {
+        MyGame().onPanUpdate(details);
+      },
+      child: MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: GameWidget(game: MyGame()),
             ),
           ),
-        ],
-      ),
-      body: Column(
-        children: [
-          CarouselSlider.builder(
-            itemCount: _apodImageUrls.length,
-            itemBuilder: (BuildContext context, int index, int currentIndex) {
-              if (index >= 0 && index < _apodImageUrls.length) {
-                return Image.network(
-                  _apodImageUrls[index],
-                  fit: BoxFit.cover,
-                  loadingBuilder: (BuildContext context, Widget child,
-                      ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                );
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-            options: CarouselOptions(
-              height: 300,
-              viewportFraction: 0.8,
-              initialPage: _apodImageUrls.length - 1,
-              enableInfiniteScroll: false,
-              reverse: true,
-              onPageChanged: (index, _) {
-                setState(() {
-                  _selectedDate =
-                      DateTime.now().subtract(Duration(days: index - 1));
-                });
-                _fetchAPOD(_selectedDate);
-              },
-            ),
-          ),
-          SizedBox(height: 16),
-          Flexible(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Text(
-                    _apodDescription,
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_apodImageUrls.isNotEmpty) {
-                        _setWallpaper(_apodImageUrls[_apodImageUrls.length -
-                            1]); // Set wallpaper with the currently displayed image
-                      }
-                    },
-                    child: Text('Set as Wallpaper'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              showDatePicker(
-                context: context,
-                initialDate: _selectedDate,
-                firstDate: DateTime(1995),
-                lastDate: DateTime.now(),
-              ).then((selectedDate) {
-                if (selectedDate != null) {
-                  setState(() {
-                    _selectedDate = selectedDate;
-                    _apodImageUrls.clear();
-                    _apodDates.clear();
-                  });
-                  _fetchAPOD(_selectedDate);
-                  for (int i = 1; i < _apodDates.length; i++) {
-                    DateTime date = _apodDates[i];
-                    _fetchAPOD(date);
-                  }
-                }
-              });
-            },
-            child: Text('Select Date'),
-          ),
-        ],
+        ),
       ),
     );
   }
